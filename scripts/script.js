105d19de51332fe7b916870c46f7d75e8550cd1b
mayo <jruso2008@gmail.com>
2023-10-09 16:46:08 -0300

    more responsive animation set

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 const menu = document.getElementById("nav__icon");
+const message = document.getElementById("messages")
 const cerrar_ventana = document.getElementById("menu__url");
 const galeria = document.getElementsByClassName("galeria")[0];
 const gal = document.getElementById("gal");
@@ -11,21 +12,37 @@ function expand(id) {
   var galarray = galeria.children;
   for (let index = 0; index < galarray.length; index++) {
     galeria.children[index].classList.remove("expand");
-  }
+  } //quitar todas las clases expand de todos los elemtto
   if (image.classList.contains("expand")) {
-    console.log(image.classList);
+    image.classList.remove("expand");
   } else {
-    image.classList.toggle("expand");
+    image.classList.add("expand");
   }
-gal.addEventListener("mousedown", ()=>{
+gal.addEventListener("mouseleave", ()=>{
+  image.classList.remove("expand")
+})
+setTimeout(()=>{
   image.classList.remove("expand")
 
-})
+},5000)
 }
 gal.addEventListener("mouseover", ()=>{
-  console.log("holaaa")
   if(flag== 0){
     flag =1
-    alert("Prueba a cliquear las imagenes!")
+    message.style.display = "flex"
+    message.style.flexDirection = "column"
+    message.style.placeContent = "center"
+    message.style.gap = "10px"
+     
+     let button = document.createElement("button")
+     button.innerText = "suka naxi"
+     button.classList.add("button")
+     button.setAttribute("onclick", "closeAd()")
+     message.innerHTML = "<p>Prueba a Cliquear las Imagenes!</p>"
+     message.style.color = "#fff"
+     message.append(button)
   }
 })
+function closeAd(){
+  message.style.display = "none"
+}
